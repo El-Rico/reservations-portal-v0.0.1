4c8cb642caebe923b73923d2a912f1a97d25695a
Rick Snackers <rsnackers@gmail.com>
2025-07-04 12:54:25 +0200

    Change layout / add header

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,9 +6,20 @@ import {
 	Dialog,
 	DialogBackdrop,
 	DialogPanel,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuItems,
 	TransitionChild,
 } from "@headlessui/react";
-import { Bars3Icon, ClockIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+	Bars3Icon,
+	BellIcon,
+	ChevronDownIcon,
+	ClockIcon,
+	MagnifyingGlassIcon,
+	XMarkIcon,
+} from "@heroicons/react/24/outline";
 import { Suspense, useState } from "react";
 
 // function classNames(...classes) {
@@ -85,52 +96,112 @@ export default function DashboardLayout({
 								<li>
 									<DashboardNavigation />
 								</li>
-								<li className="-mx-6 mt-auto">
-									<a
-										href="#"
-										className="flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-white hover:bg-gray-800"
-									>
-										<img
-											alt=""
-											src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-											className="size-8 rounded-full bg-gray-800"
-										/>
-										<span className="sr-only">Your profile</span>
-										<span aria-hidden="true">Tom Cook</span>
-									</a>
-								</li>
 							</ul>
 						</nav>
 					</div>
 				</div>
 
-				<div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-xs sm:px-6 lg:hidden">
-					<button
-						type="button"
-						onClick={() => setSidebarOpen(true)}
-						className="-m-2.5 p-2.5 text-gray-400 lg:hidden"
-					>
-						<span className="sr-only">Open sidebar</span>
-						<Bars3Icon aria-hidden="true" className="size-6" />
-					</button>
-					<div className="flex-1 text-sm/6 font-semibold text-white">
-						Dashboard
-					</div>
-					<a href="#">
-						<span className="sr-only">Your profile</span>
-						<img
-							alt=""
-							src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-							className="size-8 rounded-full bg-gray-800"
-						/>
-					</a>
-				</div>
+				<div className="lg:pl-72">
+					<div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-xs sm:gap-x-6 sm:px-6 lg:px-8">
+						<button
+							type="button"
+							onClick={() => setSidebarOpen(true)}
+							className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+						>
+							<span className="sr-only">Open sidebar</span>
+							<Bars3Icon aria-hidden="true" className="size-6" />
+						</button>
 
-				<main className="py-10 lg:pl-72">
-					<div className="px-4 sm:px-6 lg:px-8">
-						<Suspense fallback={<ClockIcon />}>{children}</Suspense>
+						{/* Separator */}
+						<div
+							aria-hidden="true"
+							className="h-6 w-px bg-gray-900/10 lg:hidden"
+						/>
+
+						<div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+							<form action="#" method="GET" className="grid flex-1 grid-cols-1">
+								<input
+									name="search"
+									type="search"
+									placeholder="Search"
+									aria-label="Search"
+									className="col-start-1 row-start-1 block size-full bg-white pl-8 text-base text-gray-900 outline-hidden placeholder:text-gray-400 sm:text-sm/6"
+								/>
+								<MagnifyingGlassIcon
+									aria-hidden="true"
+									className="pointer-events-none col-start-1 row-start-1 size-5 self-center text-gray-400"
+								/>
+							</form>
+							<div className="flex items-center gap-x-4 lg:gap-x-6">
+								<button
+									type="button"
+									className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+								>
+									<span className="sr-only">View notifications</span>
+									<BellIcon aria-hidden="true" className="size-6" />
+								</button>
+
+								{/* Separator */}
+								<div
+									aria-hidden="true"
+									className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
+								/>
+
+								{/* Profile dropdown */}
+								<Menu as="div" className="relative">
+									<MenuButton className="relative flex items-center">
+										<span className="absolute -inset-1.5" />
+										<span className="sr-only">Open user menu</span>
+										<img
+											alt=""
+											src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+											className="size-8 rounded-full bg-gray-50"
+										/>
+										<span className="hidden lg:flex lg:items-center">
+											<span
+												aria-hidden="true"
+												className="ml-4 text-sm/6 font-semibold text-gray-900"
+											>
+												Tom Cook
+											</span>
+											<ChevronDownIcon
+												aria-hidden="true"
+												className="ml-2 size-5 text-gray-400"
+											/>
+										</span>
+									</MenuButton>
+									<MenuItems
+										transition
+										className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+									>
+										<MenuItem>
+											<a
+												href="#"
+												className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
+											>
+												Menu item 1
+											</a>
+										</MenuItem>
+										<MenuItem>
+											<a
+												href="#"
+												className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
+											>
+												Menu item 2
+											</a>
+										</MenuItem>
+									</MenuItems>
+								</Menu>
+							</div>
+						</div>
 					</div>
-				</main>
+
+					<main className="py-10">
+						<div className="px-4 sm:px-6 lg:px-8">
+							<Suspense fallback={<ClockIcon />}>{children}</Suspense>
+						</div>
+					</main>
+				</div>
 			</div>
 		</>
 	);
--- components/dashboard/company-logo.tsx
+++ components/dashboard/company-logo.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function CompanyLogo() {
 	return (
 		<div className="flex gap-2 items-center text-white">
-			<Image src="/bookmark.svg" alt="Company logo" width={30} height={30} />
-			<span className="uppercase tracking-wider font-bold">Reservations</span>
+			<Image src="/bookmark.svg" alt="Company logo" width={25} height={25} />
+			<span className="uppercase font-light">Reservations</span>
 		</div>
 	);
 }
