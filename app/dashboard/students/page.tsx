845ea04cd24376c7e57c7bc68a488d92de255ef5
Rick Snackers <rsnackers@gmail.com>
2025-07-04 20:39:18 +0200

    try fix vercel build

--- app/dashboard/students/page.tsx
+++ app/dashboard/students/page.tsx
@@ -1,17 +1,18 @@
 import prisma from "@/lib/prisma";
+import { Student } from "@prisma/client";
 import { format, setDefaultOptions } from "date-fns";
 import { nl } from "date-fns/locale";
 
 setDefaultOptions({ locale: nl });
 
 export default async function DashboardStudents() {
-	const students = await prisma.student.findMany();
+	const students: Student[] = await prisma.student.findMany();
 
 	return (
 		<>
 			<h1>Students!</h1>
 			<ul>
-				{students.map((student) => (
+				{students.map((student: Student) => (
 					<li key={student.id}>
 						{student.first_name} {student.last_name} - sinds{" "}
 						{format(new Date(student.createdAt), "EEEE, d LLLL yyyy")}
