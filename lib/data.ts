969b4eb745b80374fd8db2aab2de415b65b3c29b
Rick Snackers <rsnackers@gmail.com>
2025-07-29 23:55:15 +0200

    feat: fix errors in schema

--- lib/data.ts
+++ lib/data.ts
@@ -1,32 +1,32 @@
-// import z from "zod";
-// import prisma from "./prisma";
-// import { newStudentFormSchema } from "./validators";
+import z from "zod";
+import prisma from "./prisma";
+import { newStudentFormSchema } from "./validators";
 
-// export default async function addNewStudent(
-// 	studentData: z.infer<typeof newStudentFormSchema>
-// ) {
-// 	const { lessons, classes, ...rest } = studentData;
+export default async function addNewStudent(
+	studentData: z.infer<typeof newStudentFormSchema>
+) {
+	const { lessons, classes, ...rest } = studentData;
 
-// 	const response = await prisma.student.create({
-// 		data: {
-// 			...rest,
-// 			lessons:
-// 				lessons && lessons.length > 0
-// 					? { connect: lessons.map((id) => ({ id: Number(id) })) }
-// 					: undefined,
-// 			classes: classes ? { connect: [{ id: Number(classes) }] } : undefined,
-// 		},
-// 	});
-// 	console.log("New student added:", response);
-// 	return response;
-// }
+	const response = await prisma.student.create({
+		data: {
+			...rest,
+			lessons:
+				lessons && lessons.length > 0
+					? { connect: lessons.map((id) => ({ id: Number(id) })) }
+					: undefined,
+			classes: classes ? { connect: [{ id: Number(classes) }] } : undefined,
+		},
+	});
+	console.log("New student added:", response);
+	return response;
+}
 
-// export async function getAllStudents() {
-// 	const students = await prisma.student.findMany({
-// 		include: {
-// 			lessons: true,
-// 			classes: true,
-// 		},
-// 	});
-// 	return students;
-// }
+export async function getAllStudents() {
+	const students = await prisma.student.findMany({
+		include: {
+			lessons: true,
+			classes: true,
+		},
+	});
+	return students;
+}
--- lib/types.ts
+++ lib/types.ts
@@ -3,7 +3,6 @@ import { newStudentFormSchema } from "./validators";
 
 export type Student = z.infer<typeof newStudentFormSchema> & {
 	id: string;
-	lessons: string[];
 	createdAt: Date;
 	modifiedAt: Date;
 };
